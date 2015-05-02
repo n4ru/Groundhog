33e9df38d1ca12edc99347563c01d755ced27916
Brooke Cantwell <bcantwell1@fordham.edu>
2015-05-02 19:29:12 -0400

    Styling Comment Box
    Slide up comment box and CSS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var payload = '<div class="z"><a href="about:blank"><b>1 Comment</b></a></div>';
 
-payload += '<div class="commentsBox"> <div class="comment"> <p class="commentAuthor">Gordon</p><p class="commentContent">foobar hello world.</p></div><div class="comment"> <p class="commentAuthor">Gordon</p><p class="commentContent">foobar hello world.</p></div><div class="comment"> <p class="commentAuthor">Gordon</p><p class="commentContent">foobar hello world.</p></div></div>';
+payload += '<div class="commentsBox"> <div class="comment"> <p class="commentAuthor"><a href="google.com"> Gordon </a> </p><p class="commentContent">foobar hello world.</p></div><div class="comment"> <p class="commentAuthor">Gordon</p><p class="commentContent">foobar hello world.</p></div><div class="comment"> <p class="commentAuthor">Gordon</p><p class="commentContent">foobar hello world.</p></div></div>';
 
 // Bind to new search
 console.log("We're loaded")
@@ -27,4 +27,6 @@ $(document).on('DOMNodeInserted', function(event) {
 function injectPayload() {
   $('.g').append(payload);
   $('.r').hover(function() { $('.commentsBox').slideDown(); });
+  $('.r').on('mouseleave', function(event) { $('.commentsBox').slideUp();})
+
 }
