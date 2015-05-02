c836ccfe8b0aa325ad234b24dbc35aa7d744862f
n4ru <geokoder@gmail.com>
2015-05-02 15:07:59 -0400

    Button binding.

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".lsb").on('click', function() {
+	console..log("You clicked the search button")
+});
+
+$(document).keypress(function(e) {
+	if (e.which == 13) {
+		alert('You 	pressed enter!');
+	}
+});
