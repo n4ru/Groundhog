e9a46fd4cc0cb08ff5391a1fc34a2a5753daf430
Gordon Zheng <technix1@gmail.com>
2015-05-02 18:38:54 -0400

    store injected content into payload var

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+var payload = '<div class="z"><a href="about:blank"><b>1 Comment</b></a></div>';
+
 // Bind to new search
 console.log("We're loaded")
 $(".lsb").on('click', function() {
 	console.log("You clicked the search button")
 });
-
-
 
 $(document).keypress(function(e) {
 	if (e.which == 13) {
@@ -13,13 +13,13 @@ $(document).keypress(function(e) {
 });
 
 $(document).ready(function() {
-	$('.g').append('<div class="z"><a href="about:blank"><b>1 Comment</b></a></div>');
-})
+	$('.g').append(payload);
+});
 
 // Add shit to each result.	
 $(document).on('DOMNodeInserted', function(event) {
 	if (event.target.innerHTML.indexOf('Search Results') === 23) { // Find a faster way of finding this element.
 		$('.z').remove();
-		$('.g').append('<div class="z"><a href="about:blank"><b>1 Comment</b></a></div>');
+		$('.g').append(payload);
 	}
-})
+});
