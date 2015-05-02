d65ed96f2307f22c4b150d23c853b76b05b61b72
Gordon Zheng <technix1@gmail.com>
2015-05-02 18:57:55 -0400

    add comments box

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var payload = '<div class="z"><a href="about:blank"><b>1 Comment</b></a></div>';
 
+payload += '<div class="commentsBox"> <div class="comment"> <p class="commentAuthor">Gordon</p><p class="commentContent">foobar hello world.</p></div><div class="comment"> <p class="commentAuthor">Gordon</p><p class="commentContent">foobar hello world.</p></div><div class="comment"> <p class="commentAuthor">Gordon</p><p class="commentContent">foobar hello world.</p></div></div>';
+
 // Bind to new search
 console.log("We're loaded")
 $(".lsb").on('click', function() {
@@ -12,14 +14,17 @@ $(document).keypress(function(e) {
 	}
 });
 
-$(document).ready(function() {
-	$('.g').append(payload);
-});
+$(document).ready(injectPayload);
 
 // Add shit to each result.	
 $(document).on('DOMNodeInserted', function(event) {
 	if (event.target.innerHTML.indexOf('Search Results') === 23) { // Find a faster way of finding this element.
 		$('.z').remove();
-		$('.g').append(payload);
+		injectPayload();
 	}
 });
+
+function injectPayload() {
+  $('.g').append(payload);
+  $('.r').hover(function() { $('.commentsBox').slideDown(); });
+}
