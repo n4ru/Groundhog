eedfd30f3bf1705f8eec9bd8c8f9ce8797f3b066
n4ru <geokoder@gmail.com>
2015-05-03 06:27:28 -0400

    ...

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+var host = 'https://ghog.herokuapp.com'; // Bind to new search
+/*
 var querycomments = '<div class="querycomments"> <div class="comment"> <p class="commentAuthor">Gordon</p><p class="commentContent">foobar hello world.</p></div><div class="comment"> <p class="commentAuthor">Gordon</p><p class="commentContent">foobar hello world.</p></div><div class="commentForm"> <input> <button>Submit</button> </div>';
 var featuredresults = '<div class="comment"> <p class="commentAuthor">Gordon</p><p class="commentContent">foobar hello world.</p></div><div class="comment"> <p class="commentAuthor">Gordon</p><p class="commentContent">foobar hello world.</p></div><div class="commentForm"> <input> <button>Submit</button> </div></div>';
+*/
+var upanddown = '<div> <div class="up"> <img style="height: 20px;" src="http://i.imgur.com/fI5pgx8.png"> <div class="down"> <br> <img style="height: 20px;" src="http://i.imgur.com/Oh4jt7R.png"> </div> </div>';
 
-var host = 'https://ghog.herokuapp.com'; // Bind to new search
 
 $(document).ready(injectPayload);
 
@@ -56,10 +59,10 @@ function godFuckingDamnit(context, thisrc, thisbox) {
 	$(context).click(function(e) {
 		SubmitThisShit(context, thisrc, thisbox);
 	})
-	
+
 	$(document).keyup(function(e) {
 		if ($(context).siblings("input:focus").length === 1 && (e.keyCode === 13)) {
-			SubmitThisShit(context, thisrc, thisbox);	
+			SubmitThisShit(context, thisrc, thisbox);
 		}
 	})
 
@@ -86,13 +89,29 @@ function injectPayload() {
 		}
 		commentsOpen = 1;
 		if (commentsOpen) {
-			$('.commentsBox', this).stop(true, true).slideDown(300);
+			$('.commentsBox', this).stop(true, true).slideDown();
 		}
 	});
 
 	$('.rc').on('mouseleave', function(event) {
-		self = this;
-		closeComments(self)
-	})
-
+			self = this;
+			closeComments(self)
+		})
+		//injectquerycomments()
+	injectupanddown()
 }
+
+function injectquerycomments() {
+	$('#appbar').append(querycomments + featuredresults);
+	$('#rhs_block').css("display", "none");
+	console.log($('#rhs_block'))
+}
+
+function injectupanddown() {
+	$('.rc').append(upanddown);
+}
+
+
+$('.down').click(function() {
+	$(this).css('color', 'orange');
+});
